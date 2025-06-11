c723f05bd00c38995003eda570240c067656743b
lapui <lapui@DESKTOP-8L23PR8>
2025-06-10 21:36:14 -0300

    First Commit

---
+++ src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core'
+
+@Component({
+
+     selector : 'hello-world',
+     template:  '<h1>Hello Mundo 2</h1>'
+
+})
+
+export class HelloWorldCommponent{
+
+
+}
