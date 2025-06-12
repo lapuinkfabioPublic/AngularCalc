96035072ad4e5d136e7cc1089679dbe196af5247
lapui <lapui@DESKTOP-8L23PR8>
2025-06-12 03:17:54 -0300

    Exemplo de Lista Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculadoraComponent } from "./calculadora/calculadora.component";
-
+import { ListaComprasComponent} from "./lista-compras/lista-compras.component"
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  CalculadoraComponent],
+  imports: [RouterOutlet,   ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
